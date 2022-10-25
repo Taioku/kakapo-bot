ad7e4ce1bb81642eb8f3462510ef51e61c28892a
Taioku <t.rgpns@gmail.com>
2022-10-25 02:32:00 -0300

    0.1

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import os
 intents = discord.Intents.default()
 intents.members = True
 
-token = os.environ["DISCORD_TOKEN"]
+#token = os.environ["DISCORD_TOKEN"]
+token = "MTAzMjg1NDkwMTQ1OTUyNTY3Mg.GHAYwB.FWSbN95Mm6H8Xr5W21js_MzI-8G_IKick24Uw4"
 
 kakapo = commands.Bot(command_prefix = "k!", intents=intents)
 
